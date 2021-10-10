2119eb5598a70b866a1a7de5a3030d2b5fee5c40
Javeed Suhail <javeed3157@gmail.com>
2021-10-10 17:19:08 +0530

    Added cycle 2 experiments

---
+++ myhttp.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.net.*;
+//39110926-SHAIK JAVEED SUHAIL
+public class myhttp {
+    public static void main(String args[]) throws IOException {
+        URL url = new URL("http://www.sathyabama.ac.in/");
+        URLConnection conn = url.openConnection();
+        conn.connect();
+        InputStreamReader content = new InputStreamReader(conn.getInputStream());
+        FileWriter f = new FileWriter("abc.html");
+        for (int i = 0; i != -1; i = content.read()) {
+            f.write((char) i);
+        }
+    }
+}
